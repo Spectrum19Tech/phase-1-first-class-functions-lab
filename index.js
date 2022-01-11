d44ba799f9ed0d6f6350feecac4fd710f8094348
Jonathan Bryant <spectrum19tech@gmail.com>
2022-01-11 11:39:39 -0800

    commit tree 2a5b934bbe565705bd9b32c8af85b9cf0734f3f3

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr) {
+    return arr.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(arr) {
+    return arr.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    const fareMultiplier = function(num) {
+        num = num *5
+        return num;
+    }
+    return fareMultiplier
+}
+
+function fareDoubler(rate) {
+    return rate * 2
+}
+
+function fareTripler(rate) {
+    return rate *3;
+}
+
+function selectDifferentDrivers(selectingDrivers, func) {
+    return func(selectingDrivers);
+}
